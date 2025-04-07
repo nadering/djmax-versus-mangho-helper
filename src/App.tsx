5c26f1018a85a44427ea893c32303529848e71f4
nadering <120453768+nadering@users.noreply.github.com>
2025-04-07 23:25:52 +0900

    feat: 가이드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import GuideModal from "./components/guide/GuideModal";
 import PlayerList from "./components/player-list/PlayerList";
 
 function App() {
   return (
     <div className="relative w-full h-full flex flex-col items-center bg-gray-200 py-8">
-      <p className="text-4xl font-bold select-none">버망호 도우미</p>
+      <div className="relative w-full max-w-xl flex justify-center">
+        <p className="text-4xl font-bold select-none">버망호 도우미</p>
+        <GuideModal />
+      </div>
       <PlayerList />
     </div>
   );
---
+++ src/components/guide/GuideModal.tsx
@@ -0,0 +1,48 @@
+import GuideImg from "/djmax-versus-mangho-helper-guide.png";
+import { CircleXIcon, HelpCircleIcon } from "lucide-react";
+import { useState } from "react";
+
+/** 가이드 이미지 모달 */
+const GuideModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {/* 버튼 */}
+      <button
+        className="absolute top-[50%] right-4 translate-y-[-50%] cursor-pointer duration-100 hover:scale-120"
+        onClick={() => setIsOpen(true)}
+      >
+        <HelpCircleIcon className="w-6 h-6" />
+      </button>
+
+      {/* 모달 */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 select-none"
+          onClick={() => setIsOpen(false)} // 배경 클릭 시 닫기
+        >
+          <div
+            className="bg-white rounded-lg p-4 shadow-lg max-w-4xl w-full"
+            onClick={(e) => e.stopPropagation()} // 내부 클릭 시 모달 유지
+          >
+            <div className="flex justify-end pb-1">
+              <button
+                className="text-gray-500 hover:text-black text-sm cursor-pointer"
+                onClick={() => setIsOpen(false)}
+              >
+                <CircleXIcon className="w-5 h-5" />
+              </button>
+            </div>
+            <img
+              src={GuideImg}
+              alt="가이드 이미지"
+              className="w-full h-auto rounded select-none"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default GuideModal;
